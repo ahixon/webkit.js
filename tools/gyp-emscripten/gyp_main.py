e305d0ed9bd0d170b99ec2cc0bf408f4cf29d468
Alex Hixon <alex@alexhixon.com>
2016-11-30 11:43:33 +1100

    don't import system gyp if it doesn't have js_library target support

--- tools/gyp-emscripten/gyp_main.py
+++ tools/gyp-emscripten/gyp_main.py
@@ -6,13 +6,40 @@
 
 import sys
 
-# TODO(mark): sys.path manipulation is some temporary testing stuff.
-try:
-  import gyp
-except ImportError, e:
-  import os.path
-  sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), 'pylib'))
-  import gyp
-
 if __name__ == '__main__':
+  use_internal_gyp = False
+
+  try:
+    import gyp
+    try:
+      gyp.input.ValidateTargetType (None, {'type': 'js_library'})
+    except gyp.common.GypError, e:
+      use_internal_gyp = True
+      del sys.modules['gyp']
+  except ImportError, e:
+    use_internal_gyp = True
+    del sys.modules['gyp']
+
+  if use_internal_gyp:
+    print "Using internal GYP"
+
+    # clear existing import
+    try:
+      del sys.modules['gyp']
+    except KeyError:
+      pass
+
+    # and subsequent self-referential imports
+    for mod in sys.modules.keys():
+      if mod.startswith('gyp.'):
+        del sys.modules[mod]
+
+    # add our copy to path
+    import os.path
+    sys.path.insert (0, os.path.join (os.path.join(os.path.dirname(sys.argv[0]), 'pylib')))
+    print "python path now", sys.path
+
+    # import local version
+    import gyp
+
   sys.exit(gyp.script_main())
